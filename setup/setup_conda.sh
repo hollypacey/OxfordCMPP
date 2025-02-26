226dfca02c87081bb891312f309783cbcdfe984b
Holly Pacey <holly.ann.pacey@cern.ch>
2025-02-26 09:17:04 +0000

    first part1 commit

---
+++ setup/setup_conda.sh
@@ -0,0 +1,3 @@
+# set up your conda installation
+conda create -c conda-forge --name cmpp_root_env root python=3.8
+#To be continued....
---
+++ setup/setup_venv.sh
@@ -0,0 +1,5 @@
+# flag important to use pplxint ROOT installation for ease.
+python -m venv venv --system-site-packages
+source venv/bin/activate
+pip install jupyter ipython ipykernel
+
